8833badefa40a1dbe7c956cbd4f1f3245304e230
Baileigh <bai1eigh@outlook.com>
2022-07-02 14:36:40 -0400

    changed the index.js a lot, added to readme

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,138 @@
 // TODO: Include packages needed for this application
+var inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = ['what is your the title of your project?'];
+
+// TODO: Create an array of questions for user input // Creating an array of objects: https://www.freecodecamp.org/news/javascript-array-of-objects-tutorial-how-to-create-update-and-loop-through-objects-using-js-array-methods/
+const questions = [
+
+
+    //Question for title
+{name: 'title',
+type:'input',
+message:'What is your the title of your project?',
+default:'Project Title',
+},
+
+  //Question for description
+{name: 'Description',
+type:'input',
+message:'Please enter a description for your application.',
+default:'Description',
+},
+
+  //Question for contributers
+{name: 'contributers',
+type: 'input',
+default: 'Project contributers',
+message:'Who are the contributers of this application?',
+},
+
+  //Question for license
+{
+
+    name: 'Project License',
+    type:'list',
+    message: 'Would you like to display the license of the project?',
+    choices:  
+    ['GNU AGPLv3', 
+    'GNU GPLv3',
+     'GNU LGPLv3', 
+     'Mozilla Public License 2.0',
+    'Apache License 2.0', 
+    'MIT License', 
+    'Boost Software License 1.0',
+    'The Unlicense'],
+    
+
+},
+
+  //Question for table of contents
+{
+
+    name:'Table of Contents',
+    type:'input',
+    message: 'Does your application require a table of contents?',
+    
+},
+
+
+  //Question for usage
+
+{
+    name: 'usage',
+    type: 'input',
+    message: 'What is the intended usage of your application?',
+    
+},
+
+
+  //Question for installation
+{
+    name: 'Insatllation',
+    type: 'input',
+    message: 'How can someone install this project?',
+    
+},
+
+  //Question for information
+{
+    name: 'Information',
+    type: 'input',
+    message: 'Would you like to enter some information about how to use your project?',
+    
+},
+
+  //Question for testing
+{
+    name: 'Testing',
+    type: 'input',
+    message:'How is this project tested?',
+    
+},
+
+  //Question for issues
+{
+    name: 'Issues',
+    type: 'input',
+    message: 'What do I do if I have an issue?',
+    
+},
+//username
+{
+    type: "input",
+    name: "username",
+    message: "Please enter your GitHub username: "
+},
+
+//email
+{
+    type: "input",
+    name: "email",
+    message: "Please enter your email: "
+}
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName,generateMarkdown(data), function (err) {
+        if (err) {
+            return console.log(err)
+        }
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+    inquirer.prompt(questions).then((data) => {
+        console.log(JSON.stringify(data, null, " "));
+        data.getLicense = getLicense(data.license);
+       writeToFile('./README.md',data);
+    });
+}
 
 // Function call to initialize app
 init();
--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # Professional README Generator Starter Code
 
+# <project-title>
+
+## Description
 [How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+
+## Table of Contents 
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [title](#project-title)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Testing](#tests)
+- [contributions](#contributions)
+- [information](#information)
+
+
+## insallation
+
+## usage
+
+## information
+
+## contributions
+
+## tests
+
+## license
